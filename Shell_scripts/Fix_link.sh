fc86fa2db7f9ab079c326a5082ed10c638786deb
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-06-03 07:09:47 +0800

    第一次commit

---
+++ Shell_scripts/Fix_link.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+#Script名稱：自訂kernel header安裝後修復symbolic link用script
+#Script作者：林博仁
+#智慧財產授權條款：GPLv3 or later
+
+MODULEDIR=/lib/modules/`uname -r`
+HEADERDIR=/usr/src/linux-headers-`uname -r`
+
+# exit silently if there is no module dir
+test -d $MODULEDIR || exit 1
+test -d $HEADERDIR || exit 2
+
+# update build link -- by first deleting whatever is there
+test ! -e $MODULEDIR/build || rm -f $MODULEDIR/build
+# and then creating a new one
+ln --symbolic --force $HEADERDIR $MODULEDIR/build
