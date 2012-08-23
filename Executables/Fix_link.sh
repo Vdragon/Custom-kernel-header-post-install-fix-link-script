40e39c5e92d5f0d55e1a810805573c366971dc06
Ｖ字龍(Vdragon) <pika1021@gmail.com>
2012-08-23 23:50:06 +0800

    Make variable more readable

--- Executables/Fix_link.sh
+++ Executables/Fix_link.sh
@@ -2,18 +2,18 @@
 #Script名稱：自訂kernel header安裝後修復symbolic link用script
 #智慧財產授權條款：GPLv3 or later
 
-MODULEDIR=/lib/modules/`uname -r`
-HEADERDIR=/usr/src/linux-headers-`uname -r`
+module_root_directory=/lib/modules/$(uname -r)
+header_root_directory=/usr/src/linux-headers-$(uname -r)
 
 # exit silently if there is no module dir
-test -d $MODULEDIR || exit 1
-test -d $HEADERDIR || exit 2
+test -d ${module_root_directory} || exit 1
+test -d ${header_root_directory} || exit 2
 
 # update links -- by first deleting whatever is there
-test ! -e $MODULEDIR/build || rm --force $MODULEDIR/build
-test ! -e $MODULEDIR/source || rm --force $MODULEDIR/source
+test ! -e ${module_root_directory}/build || rm --force ${module_root_directory}/build
+test ! -e ${module_root_directory}/source || rm --force ${module_root_directory}/source
 
 # and then creating a new one
 # (in my test build and source should links to the same header folders
-ln --verbose --symbolic --force $HEADERDIR $MODULEDIR/build
-ln --verbose --symbolic --force $HEADERDIR $MODULEDIR/source
+ln --verbose --symbolic --force ${header_root_directory} ${module_root_directory}/build
+ln --verbose --symbolic --force ${header_root_directory} ${module_root_directory}/source
